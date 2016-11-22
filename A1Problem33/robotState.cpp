3d0638c5fd5065cf82f286305057eed44424bf40
LTaylor <darting.thoughts@gmail.com>
2016-11-22 13:38:33 -0800

    Update robotState.cpp

--- A1Problem33/robotState.cpp
+++ A1Problem33/robotState.cpp
@@ -17,3 +17,22 @@ int robotState::getArmPosition() {
 int robotState::getScore() {
     return score;
 }
+void Drive(int distance, int position) {            
+    position += distance;
+    std::cout << "Robot is now at position" << position << std::endl;
+}
+void MoveArmT(int position, int armPosition) {
+    armPostition = position
+    std::cout << "Robot arm is now at position" << armPosition << std::endl;
+}
+/*void ScorePiece() {
+    if (robot.hasPiece() && robot.position == 6 && robot.armPosition == 10) {
+        robot.score += 1;
+        std::cout << "AYy!! Your robot scored a point!" << std::endl;
+    }else{
+        std::cout << "Huh? Your robot can't score!" << std::endl;
+    }   
+}*/
+void PickUpPiece() {
+           
+}
