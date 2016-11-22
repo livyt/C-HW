43b2c43b248a7016dc7d03105ff034d6fc4b6286
LTaylor <darting.thoughts@gmail.com>
2016-11-22 13:37:59 -0800

    Update main.cpp

--- A1Problem33/main.cpp
+++ A1Problem33/main.cpp
@@ -10,22 +10,4 @@ int main() {
     return 0;
 }
 
-int Drive(int distance, int position) {            
-    position += distance;
-    std::cout << "Robot is now at position" << position << std::endl;
-}
-void MoveArmT(int position, int armPosition) {
-    armPostition = position
-    std::cout << "Robot arm is now at position" << armPosition << std::endl;
-}
-/*void ScorePiece() {
-    if (robot.hasPiece() && robot.position == 6 && robot.armPosition == 10) {
-        robot.score += 1;
-        std::cout << "AYy!! Your robot scored a point!" << std::endl;
-    }else{
-        std::cout << "Huh? Your robot can't score!" << std::endl;
-    }   
-}*/
-void PickUpPiece() {
-           
-}
+
