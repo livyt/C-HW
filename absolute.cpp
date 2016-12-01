063986c35e7adbafaeaad6fb4dda75e123044b5f
javaraser <livytaylor47@gmail.com>
2016-12-01 01:42:28 +0000

    Fixed absolute.cpp/h

--- absolute.cpp
+++ absolute.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include <cmath>
+#include "absolute.h"
 int main() {
     std::cout << "NUMBER???????" << std::endl;
     double number;
     std::cin >> number;
-    if (number == 0) {
-        std::cout << number << std::endl;
-    }else {
-        number = pow(pow(number, 2), 0.5);
-        std::cout << number << std::endl;
-    }
+    std::cout << "The absolute value of your number is " << absolute(number) << std::endl;
     return 0;
+}
+int absolute(int number){
+    if (number >= 0){
+        return number;
+    }else if (number < 0){
+        return number * -1;
+    }
 }
