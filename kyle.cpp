5bef048bd6cde9d5aca54b5483a0117c519957f0
javaraser <livytaylor47@gmail.com>
2016-12-01 03:56:16 +0000

    wheres the homework

---
+++ kyle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    bool kyleMadeTheHomework = false;
+    while( !kyleMadeTheHomework ){
+        std::cout << "kyle where's the homework?!" << std::endl; 
+    }
+    return 0;
+}
