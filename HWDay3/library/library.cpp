0c7fee6a8d68dc11c04f6ecad5b838573c58d81a
javaraser <livytaylor47@gmail.com>
2016-12-03 20:52:35 +0000

    2 functions so far

--- HWDay3/library/library.cpp
+++ HWDay3/library/library.cpp
@@ -1,1 +1,24 @@
+#include "library.h"
 #include <iostream>
+#include <vector>
+
+//DATE TIME CHECKED OUT TIME TIME DATE TIME CHECKED OUT FOR DATE CHECKED OUT!!!!!! !!!!
+void Library::addPatron(std::string inputName){
+    Patron patron;
+    patron.name = inputName;
+    patrons.push_back(patron); //patrons is that vector of you-know-whats
+}
+void Library::checkOutBook(Patron patron, std::string book){
+    bool checkedOut = false;
+    for (Book i : allBooks) {
+        if (book == i.title){
+            patron.checkedOutBooks.push_back(i);
+            checkedOut = true;
+        }
+    }
+    if (!checkedOut){
+        Book newBook;
+        newBook.title = book;
+        patron.checkedOutBooks.push_back(newBook);
+    }
+}
--- HWDay3/library/library.h
+++ HWDay3/library/library.h
@@ -1,6 +1,7 @@
 #ifndef LIBRARY_H_
 #define LIBRARY_H_
 #include <vector>
+#include <string>
 class Library{
     public:
         class Book{
@@ -10,11 +11,18 @@ class Library{
             private:
                 bool checkedOut;
                 int checkedOutFor; //how long it's been checked out for
-        }
+        };
         class Patron{
             public:
                 std::vector<Book> checkedOutBooks;
-        }
-        void addPatron();
-}
+                std::string name;
+        };
+        std::vector<Patron> patrons;
+        std::vector<Book> allBooks;
+        
+        void addPatron(std::string);
+        void checkOutBook(Patron, std::string);//if the book doesn't exist it makes a book lol
+        void isBookOverdue(Book);
+        bool doesBookExist(std::string);
+};
 #endif
