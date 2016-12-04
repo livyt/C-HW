2eade73a9f67fd2a282f6c598bc505303979b3aa
javaraser <livytaylor47@gmail.com>
2016-12-04 19:46:42 +0000

    wprk

--- HWDay3/library/library.cpp
+++ HWDay3/library/library.cpp
@@ -2,31 +2,58 @@
 #include <iostream>
 #include <vector>
 
-//DATE TIME CHECKED OUT TIME TIME DATE TIME CHECKED OUT FOR DATE CHECKED OUT!!!!!! !!!!
-void Library::addPatron(std::string inputName){
-    Patron patron;
-    patron.name = inputName;
-    patrons.push_back(patron); //patrons is that vector of you-know-whats
+// DATE TIME CHECKED OUT TIME TIME DATE TIME CHECKED OUT FOR DATE CHECKED
+// OUT!!!!!! !!!!
+void Library::addPatron(std::string inputName) {
+  Patron patron;
+  patron.name_ = inputName;
+  patrons_.push_back(patron); // patrons is that vector of you-know-whats
+  commands_executed_ += 1;
 }
-void Library::checkOutBook(Patron patron, std::string book){
-    bool checkedOut = false;
-    for (Book i : allBooks) {
-        if (book == i.title){
-            patron.checkedOutBooks.push_back(i);
-            checkedOut = true;
-        }
+void Library::checkOutBook(Patron patron, std::string book) {
+  bool checkedOut = false;
+  
+  //asking for date
+  int day;
+  int month;
+  int year;
+  Date bookDate;
+  std::cout << "hey. what day. numher" << std::endl;
+  std::cin >> day;
+  bookDate.day_ = day;
+  std::cout << "month. number" << std::endl;
+  std::cin >> month;
+  bookDate.month_ = month;
+  std::cout << "yyear." << std::endl;
+  std::cin >>  year;
+  bookDate.year_ = year;
+  
+  for (Book i : all_books_){
+    if (doesBookExist(book)){
+      i.date_checked_out_ = bookDate;
+      patron.checked_out_books_.push_back(i);
+      checkedOut = true;
     }
-    if (!checkedOut){
-        Book newBook;
-        newBook.title = book;
-        patron.checkedOutBooks.push_back(newBook);
-    }
+  }
+  if (!checkedOut) {
+    Book newBook;
+    newBook.title_ = book;
+    newBook.date_checked_out_ = bookDate;
+    patron.checked_out_books_.push_back(newBook);
+  }
+  commands_executed_ += 1;
+
 }
-bool doesBookExist(std::string search){
-    for (Book i : allBooks) {
-        if (i.title == search) {
-            return true;
-        }
+bool Library::doesBookExist(std::string search) {
+  commands_executed_ += 1;
+  for (Book i : all_books_) {
+    if (i.title_ == search) {
+      return true;
     }
-    return false;
+  }
+  return false;
+}
+bool Library::isBookOverdue(Book book){ //every 3 commands the day is laging
+  
+  return true;
 }
--- HWDay3/library/library.h
+++ HWDay3/library/library.h
@@ -2,27 +2,41 @@
 #define LIBRARY_H_
 #include <vector>
 #include <string>
-class Library{
-    public:
-        class Book{
-            public:
-                std::string title;
-                int dateCheckedOut;
-            private:
-                bool checkedOut;
-                int checkedOutFor; //how long it's been checked out for
-        };
-        class Patron{
-            public:
-                std::vector<Book> checkedOutBooks;
-                std::string name;
-        };
-        std::vector<Patron> patrons;
-        std::vector<Book> allBooks;
-        
-        void addPatron(std::string);//
-        void checkOutBook(Patron, std::string);//if the book doesn't exist it makes a book lol
-        void isBookOverdue(Book);
-        bool doesBookExist(std::string);//
+
+class Library {
+public:
+ class Book {
+  public:
+   std::string title_;
+   Date date_checked_out_;
+
+  private:
+   bool checked_out_;
+   int checked_out_for_; // how long it's been checked out for
+ };
+
+ class Patron {
+  public:
+   std::vector<Book> checked_out_books_;
+   std::string name_;
+ };
+ 
+ class Date {
+  public:
+   int date_;
+   int month_;
+   int year_;
+ };
+
+ std::vector<Patron> patrons_;
+ std::vector<Book> all_books_;
+ 
+ void addPatron(std::string); //
+ void checkOutBook(Patron, std::string); // if the book doesn't exist it makes a book lol
+ bool isBookOverdue(Book);
+ bool doesBookExist(std::string); //
+ 
+private:
+ int commands_executed_ = 0;
 };
-#endif
+#endif // LIBRARY_H_
