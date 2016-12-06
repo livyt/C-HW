6db587f91c1e336a0b5407d3b59fd1f648755aad
javaraser <livytaylor47@gmail.com>
2016-12-06 00:37:04 +0000

    I have no clue what I'm doing

---
+++ HWDay3/math/math.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <array>
+
+/*double * cap_array(double arr[], double min, double max){
+  for (int i : (sizeof(arr)/sizeof(arr[0]))){
+    if (arr[i] < min){
+      arr[i] = min;
+    }
+  }
+}*/
+
+/*void cap_array(double* arr[], double, double_ {
+  for (double i 
+}*/
---
+++ HWDay3/math/math.h
@@ -0,0 +1,12 @@
+#include <array>
+
+namespace mmath{
+//double cap_double(double, double, double);
+//double * cap_array(double arr[], double, double);
+//void cap_array(double* arr[], double, double);
+double avg_array(double arr[]);
+bool sign(double);
+double median_machine(double arr[]);
+//double mode(std::array[double]);
+//std::array[] difference(std::array[double], std::array[double]);
+}
