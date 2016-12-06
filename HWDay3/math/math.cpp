28ac8d56e414fd4fb43b6d4f84f3e517f66e99f4
javaraser <livytaylor47@gmail.com>
2016-12-06 04:15:07 +0000

    the not-array functions are done lol

--- HWDay3/math/math.cpp
+++ HWDay3/math/math.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <array>
-
+#include "math.h"
 /*double * cap_array(double arr[], double min, double max){
   for (int i : (sizeof(arr)/sizeof(arr[0]))){
     if (arr[i] < min){
@@ -12,3 +12,17 @@
 /*void cap_array(double* arr[], double, double_ {
   for (double i 
 }*/
+
+double cap_double(double input, double min, double max){
+  if (input < min) {
+    return min;
+  }else if (input > max) {
+    return max;
+  }else{
+    return input;
+  }
+}
+
+bool sign(double input){ //true=positive false=negative
+ return (!input < 0);
+}
--- HWDay3/math/math.h
+++ HWDay3/math/math.h
@@ -1,12 +1,15 @@
+#ifndef MATH_H_
+#define MATH_H_
 #include <array>
 
 namespace mmath{
-//double cap_double(double, double, double);
+double cap_double(double, double, double);
 //double * cap_array(double arr[], double, double);
 //void cap_array(double* arr[], double, double);
-double avg_array(double arr[]);
+//double avg_array(double arr[]);
 bool sign(double);
-double median_machine(double arr[]);
-//double mode(std::array[double]);
-//std::array[] difference(std::array[double], std::array[double]);
+//double median_machine(double arr[]);
+//double mode(double arr[]);
+//std::array[] difference(double arr1[], double arr2[]);
 }
+#endif //MATH_H_
